3acf4a1520931ebbb2cf4a8b2d5ef6c398293190
Pichayoot Tanasinanan <pichayoot.pong@gmail.com>
2024-10-20 20:41:38 +0700

    Update pricing_test.py

--- pricing_test.py
+++ pricing_test.py
@@ -1,33 +1,29 @@
 import unittest
-
-from django.utils.translation.trans_real import catalog
-
-from movie import Movie, MovieCatalog
-from pricing import get_price_for_movie, NEW_RELEASE, REGULAR, CHILDREN
+from movie import Movie
+from pricing import get_price_for_movie
 
 class TestPricing(unittest.TestCase):
 
+    def setUp(self):
+        self.new_release_movie = Movie(title="Young Woman and the Sea", year=2024, genre=["Drama", "Adventure"])
+        self.regular_movie = Movie(title="Top Gun: Maverick", year=2022, genre=["Action"])
+        self.childrens_movie = Movie(title="The Legend of Sarila", year=2013, genre=["Animation", "Children"])
+
     def test_regular_price(self):
-        catalog_1 = MovieCatalog()
-        movie = catalog_1.get_movie("Top Gun: Maverick")
         days = 3
-        price = get_price_for_movie(movie, days)
+        price = get_price_for_movie(self.regular_movie, days)
         expected_price = 2.0 + 1.5 * (days - 2)
         self.assertEqual(price, expected_price)
 
     def test_childrens_price(self):
-        catalog_2 = MovieCatalog()
-        movie = catalog_2.get_movie("The Legend of Sarila")
         days = 4
-        price = get_price_for_movie(movie, days)
+        price = get_price_for_movie(self.childrens_movie, days)
         expected_price = 1.5 + 1.5 * (days - 3)
         self.assertEqual(price, expected_price)
 
     def test_new_release_price(self):
-        catalog_3 = MovieCatalog()
-        movie = catalog_3.get_movie("Young Woman and the Sea")
         days = 4
-        price = get_price_for_movie(movie, days)
+        price = get_price_for_movie(self.new_release_movie, days)
         expected_price = 3 * days
         self.assertEqual(price, expected_price)
 
